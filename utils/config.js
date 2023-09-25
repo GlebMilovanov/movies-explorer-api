b5d44dbd64bf3886ec4a04e1cb04c27fd7ebcf6d
Gleb Milovanov <milgleb@gmail.com>
2023-09-25 18:48:48 +0300

    fix: db url

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/mestodb';
+const DATABASE_URL = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : 'mongodb://localhost:27017/bitfilmsdb';
 const JWT_SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret';
 const MONGO_OPTIONS = {
   useNewUrlParser: true,
